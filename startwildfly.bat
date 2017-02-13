c3688602d298888c1227a5a850d9f84013a55e59
petermcj <petermcj@gmail.com>
2017-02-13 17:04:05 +0100

    Update startwildfly.bat

--- startwildfly.bat
+++ startwildfly.bat
@@ -1,6 +1,1 @@
-@REM set "SERVER_OPTS=--server-config=standalone-full.xml"
-@REM set "SERVER_OPTS=--server-config=standalone-full.xml"
-                          
 call target\wildfly-8.2.0.Final\bin\standalone.bat  --debug -c standalone-full.xml
-     
- No newline at end of file
